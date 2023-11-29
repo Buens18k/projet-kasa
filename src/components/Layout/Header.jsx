8e368dbf8be7fda317669767299b075e6ac6b546
Bruno Vang <buens@outlook.fr>
2023-11-29 16:24:46 +0100

    create feature header

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,14 @@
+// import { Form } from "react-router-dom"
+import logo from '../../assets/LOGO.png'
+
+function Header () {
+    return (
+        <div>
+            <img src={logo} alt="Logo KASA" />
+            <p>Ici le header</p>
+        </div>
+
+    )
+}
+
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Home from './pages/Home/Home.jsx'
-import './index.css'
+import './utils/style/index.css'
+
+const router = createBrowserRouter([{
+  path:"/",
+  element:<Home/>,
+}])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
+    <RouterProvider router={router}/>
+  </React.StrictMode>
 )
