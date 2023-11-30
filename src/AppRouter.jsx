e35edbabf5f17d17c02228a16363dd26c2c309da
Bruno Vang <buens@outlook.fr>
2023-11-30 14:23:10 +0100

    create route house, about

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import ErrorPage from './pages/Error-page';
+import House from './pages/House';
+import About from './pages/About';
 
 const AppRouter = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: '/house',
+    element: <House />,
+  },
+  {
+    path: '/about',
+    element: <About />,
   },
 ]);
 
---
+++ src/pages/House.jsx
@@ -0,0 +1,5 @@
+export default function House() {
+    return(
+        <div>Ici la page qui va fficher le logement en question (cliqué)</div>
+    )
+}
