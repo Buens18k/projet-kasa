23a44e723e973cf948cd23bd72fe67a375a583b6
Bruno Vang <buens@outlook.fr>
2023-12-01 10:32:04 +0100

    img/title Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,11 @@
 import style from '../styles/layouts/_home_location.module.scss';
-import { Link } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 export default function Card({ card }) {
   return (
     <article className={style.home__location__card}>
-      <Link to={`/house/${card.id}`} />
-      <div className={style.home__location__card__cta}>
-        <div className={style.home__location__card__cta__gradient}></div>
-        {/* <img src={card.cover} alt={card.title} /> */}
-      </div>
+        <img className={style.home__location__card__img} src={card.cover} alt={card.title} />
+        <h2 className={style.home__location__card__title}>{card.title}</h2>
     </article>
   );
 }
