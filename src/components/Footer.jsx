215816e9b0fb00599b79a79d00dd7fd205b56db1
Bruno Vang <buens@outlook.fr>
2023-12-01 11:03:32 +0100

    logo footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ export default function Footer() {
   return (
     <footer>
       <div>
+        <img src="../assets/logo_footer.png" alt="" />
         <p>Ici le Footer</p>
       </div>
     </footer>
