5799e8a1302bf119598d49143ac65516e23867e2
Bruno Vang <buens@outlook.fr>
2024-09-30 23:42:13 +0200

    configuration github Page

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -9,39 +9,44 @@ import House, { loader as houseloader } from './pages/House';
 import About from './pages/About';
 
 // Création du Routeur en utilisant la fonction 'createBrowserRouter' qui définit les différentes routes et config de l'app
-const AppRouter = createBrowserRouter([
+const AppRouter = createBrowserRouter(
+  [
+    {
+      // Configuration pour la route principal
+
+      // Si l'URL correspond à '/'
+      path: '/',
+      // Le composant 'HeaderFooterLayout' est affiché
+      element: <HeaderFooterLayout />,
+      // En cas d'erreur, le composant ‘ErrorPage’ est affiché
+      errorElement: <ErrorPage />,
+      // Contient des enfants (définit des sous-routes pour cette route principal)
+      children: [
+        {
+          // Route Racine de l'app
+
+          // Si l'URL correspond à '/'
+          path: '/',
+          // Le composant 'Home' sera affiché
+          element: <Home />,
+          // Appel la fonction alias 'homeloader' pour charger les données avant d'afficher le contenu
+          loader: homeloader,
+        },
+        {
+          path: '/house/:title',
+          element: <House />,
+          loader: houseloader,
+        },
+        {
+          path: '/about',
+          element: <About />,
+        },
+      ],
+    },
+  ],
   {
-    // Configuration pour la route principal
-
-    // Si l'URL correspond à '/'
-    path: '/',
-    // Le composant 'HeaderFooterLayout' est affiché
-    element: <HeaderFooterLayout />,
-    // En cas d'erreur, le composant ‘ErrorPage’ est affiché
-    errorElement: <ErrorPage />,
-    // Contient des enfants (définit des sous-routes pour cette route principal)
-    children: [
-      {
-        // Route Racine de l'app
-
-        // Si l'URL correspond à '/'
-        path: '/',
-        // Le composant 'Home' sera affiché
-        element: <Home />,
-        // Appel la fonction alias 'homeloader' pour charger les données avant d'afficher le contenu
-        loader: homeloader,
-      },
-      {
-        path: '/house/:title',
-        element: <House />,
-        loader: houseloader,
-      },
-      {
-        path: '/about',
-        element: <About />,
-      },
-    ],
-  },
-]);
+    basename: '/projet-kasa',
+  }
+);
 
 export default AppRouter;
